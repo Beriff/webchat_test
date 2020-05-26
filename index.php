9150952d308446bb9f0423bea1dacb59ee807c7a
Beriff <58127431+Beriff@users.noreply.github.com>
2020-05-26 10:13:51 +0700

    added login system; removed database

--- index.php
+++ index.php
@@ -1,42 +1,86 @@
 <?php
 	session_start();
-
-	$servername = "localhost";
-	$username = "db_admin";
-	$password = "qwertorros7089";
-
-	$conn = new mysqli($servername, $username, $password);
-
-	if($conn -> connect_error) {
-		die("you failed: " . $conn -> connect_error);
+	if(!isset($_SESSION['localName'])) {
+		$_SESSION['localName'] = NULL;
 	}
-	echo "database connected<br/>";
+	if(!isset($_SESSION['separator'])) {
+		$_SESSION['separator'] = " > ";
+	}
+	$local_name = $_SESSION['localName'];
+	$filename = "names.txt";
+	$filename = fopen($filename, "a+") or die();
+
+	$names = fread($filename, filesize("names.txt"));
+	$namesArray = explode("!", $names);
+	fclose($filename);
+
+	$filename = "passwords.txt";
+	$filename = fopen($filename, "a+") or die();
+
+	$passwords = fread($filename, filesize("passwords.txt"));
+	$passArray = explode("!", $passwords);
+	fclose($filename);
+
+	$accounts = array_combine($namesArray, $passArray);
+
+	echo 'Register or Login:
+	<form action="index.php" method="post"> 
+		<label for="name">Name:</label>
+		<input type="text" name="name" id="name"/><br/>
+		<label for="pass">Password:</label>
+		<input type="text" name="pass" id="pass"/><br/>
+		<input type="submit" name="sub" id="sub">
+		<hr>
+	</form>';
+	if(isset($_POST['sub'])) {
+		$local_name = $_POST['name'];
+		$local_pass = $_POST['pass'];
+		if(strpos($local_pass, "!") !== false) {
+			echo "<span style='color: red;'>Invalid Password Type! No '!' allowed!</span><br/>";
+			$local_pass = "";
+		}
+		if(array_key_exists($local_name, $accounts)) {
+			if($accounts[$local_name] == $local_pass) {
+				echo "<span style='color: green;'>Successfully logged in.</span><br/>";
+				$_SESSION['localName'] = $local_name;
+				
+			}
+			else {
+				echo "<span style='color: red;'>Invalid Password!</span><br/>";
+			}
+		}
+		else {
+			echo "<span style='color: green;'>No accounts has been found. Successfully made a new one!</span><br/>";
+			$filename = "names.txt";
+			$filename = fopen($filename, "a+") or die();
+			$local_name = $local_name."!";
+			fwrite($filename, $local_name);
+			fclose($filename);
+
+			$filename = "passwords.txt";
+			$filename = fopen($filename, "a+") or die();
+			$local_pass = $local_pass."!";
+			fwrite($filename, $local_pass);
+			fclose($filename);
+		}
+
+	}
 ?>
 
 <script>
 	function myFunction() {
-  			
-  	var copyText = document.querySelector("#toCopy");
-
-  			
-  	copyText.select();
-  	copyText.setSelectionRange(0, 99999); 
-
-  	document.execCommand("copy"); 
 	}
 </script>
 
 <?php
-	echo 'previous name:<input id="toCopy" value='.$_SESSION['localName']."'></span>";
-	echo "<button onclick='myFunction()'>Copy name</button>";
+	echo 'logged as '.$_SESSION['localName'];
 	$filename = "log.txt";
 	$filename = fopen($filename, "a+") or die(); #opening log.txt to show it on page laterly
 	$local_name = NULL;
 	$separator = " > ";
+	echo '<br/>(write /logout in message to log out)';
 	echo '
 	<form action="index.php" method="post"> 
-		<label for="name">Name:</label>
-		<input type="text" name="name" id="name"/><br/>
 		<label for="msg_hold">Message:</label>
 		<input type="text" name="msg_hold" id="msg_hold"/><br/>
 		<input type="submit" name="enter" id="enter">
@@ -55,10 +99,9 @@
 		
 		<?php
 			if(isset($_POST['enter'])) { #if user pressed "enter" button
-				if($_POST['name'] != "" and $_POST['name'] != "admin") { #if name isn't empty
-					$local_name = stripslashes(htmlspecialchars($_POST['name'])); #getting name from <input> tag
+				if(isset($_SESSION['localName'])) { 
 					$local_msg = stripslashes(htmlspecialchars($_POST['msg_hold'])); #getting message content
-					$_SESSION['localName'] = $local_name;
+					$local_name = $_SESSION['localName'];
 
 					###MESSAGE PARSER###
 
@@ -124,6 +167,12 @@
 							echo "<script type='text/javascript'>alert('separator is too long.');</script>";
 						}
 					};
+
+					if($local_msg == "/logout") {
+						$local_msg == "[USER LOGGED OUT]";
+						session_destroy();
+						header("refresh:0");
+					};
 					
 
 					$local_msg = $local_name.$_SESSION['separator'].$local_msg."<br/>"; #transforming message into NAME > MESSAGE format
@@ -132,7 +181,7 @@
 
 				}
 				else {
-					echo "<span style='color: red;'>Invalid Name, yo!</span><br/>"; #Showing if name format is invalid
+					echo '<script type="text/javascript">alert("You are not logged in.")</script>';
 				}
 			}
 			$messages = fread($filename, filesize("log.txt"));
